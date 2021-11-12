e59cbbfaf93a54982574a2ba73315ba8b9282710
Saikrishna_Dsk <saikrishnadosapati@gmail.com>
2021-11-11 22:52:35 -0800

    Login onChange Validation

--- client/components/SupplierLoginForm/SupplierLoginForm.tsx
+++ client/components/SupplierLoginForm/SupplierLoginForm.tsx
@@ -21,15 +21,15 @@ export const SupplierLoginForm: React.FC<{login: (detail: any) => void}> = ({log
                 {/* ERROR */}
                 <div className="form-group">
                     <label htmlFor="name">Name: </label>
-                    <input type="text" name="name" id="name" />
+                    <input type="text" name="name" id="name" onChange={e => setDetails({...details, name: e.target.value})} value={details.name} />
                 </div>
                 <div className="form-group">
                     <label htmlFor="email">Email: </label>
-                    <input type="text" name="email" id="email" />
+                    <input type="text" name="email" id="email" onChange={e => setDetails({...details, email: e.target.value})} value={details.email}  />
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password: </label>
-                    <input type="password" name="password" id="password" />
+                    <input type="password" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value={details.password}  />
                 </div>
                 <input type="submit" value="Login" />
             </div>
